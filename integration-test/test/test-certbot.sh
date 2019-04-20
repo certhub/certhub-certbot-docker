95b9a0f51c6044decc7439a0ec5ef7159149a07d
znerol <lo+github@znerol.ch>
2019-04-20 23:49:07 +0200

    chore(ci): Fix test script

--- integration-test/test/test-certbot.sh
+++ integration-test/test/test-certbot.sh
@@ -4,8 +4,8 @@ set -e
 set -u
 set -x
 
-git gau-ac
-git gau-xargs -I"{WORKDIR}"
-certhub-message-format "${CERTHUB_CERT_PATH}" x509
-certhub-certbot-run "${CERTHUB_CERT_PATH}" "${CERTHUB_CSR_PATH}"
+git gau-ac \
+git gau-xargs -I"{WORKDIR}" \
+certhub-message-format "${CERTHUB_CERT_PATH}" x509 \
+certhub-certbot-run "${CERTHUB_CERT_PATH}" "${CERTHUB_CSR_PATH}" \
 certbot ${CERTHUB_CERTBOT_ARGS} --config "${CERTHUB_CERTBOT_CONFIG}"
