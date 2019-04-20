314ddfa0b8115ec175a6fed8dbfd4148f32c483d
znerol <lo+github@znerol.ch>
2019-04-20 23:55:44 +0200

    chore(ci): Fix certbot config

--- integration-test/test/config-setup-certbot.sh
+++ integration-test/test/config-setup-certbot.sh
@@ -13,10 +13,12 @@ CONFIG_CERTBOT_INI_DIR=$(dirname "${CONFIG_CERTBOT_INI_PATH}")
 mkdir -p "${CONFIG_CERTBOT_INI_DIR}"
 
 cat <<EOF > "${CONFIG_CERTBOT_INI_PATH}"
---manual
---manual-auth-hook /usr/lib/certhub/certbot-hooks/lexicon-auth
---manual-cleanup-hook /usr/lib/certhub/certbot-hooks/lexicon-cleanup
---manual-public-ip-logging-ok
---preferred-challenges dns-01
---server https://acme-staging-v02.api.letsencrypt.org/directory
+server=https://acme-staging-v02.api.letsencrypt.org/directory
+manual=true
+manual-public-ip-logging-ok=true
+manual-auth-hook=/usr/local/lib/certhub/certbot-hooks/lexicon-auth
+manual-cleanup-hook=/usr/local/lib/certhub/certbot-hooks/lexicon-cleanup
+preferred-challenges=dns-01
+agree-tos=true
+register-unsafely-without-email=true
 EOF
