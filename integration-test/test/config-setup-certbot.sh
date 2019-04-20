85f2acebb9c10b9baac5ba4539180e429bb1711d
znerol <lo+github@znerol.ch>
2019-04-21 00:07:48 +0200

    chore(ci): Add integration-test (#1)

---
+++ integration-test/test/config-setup-certbot.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+set -u
+set -x
+
+CONFIG_CSR_DIR=$(dirname "${CONFIG_CSR_PATH}")
+mkdir -p "${CONFIG_CSR_DIR}"
+openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:P-256 | \
+    openssl req -new -subj "/CN=${CONFIG_CERT_CN}/" -key - -out "${CONFIG_CSR_PATH}"
+
+CONFIG_CERTBOT_INI_DIR=$(dirname "${CONFIG_CERTBOT_INI_PATH}")
+mkdir -p "${CONFIG_CERTBOT_INI_DIR}"
+
+cat <<EOF > "${CONFIG_CERTBOT_INI_PATH}"
+server=https://acme-staging-v02.api.letsencrypt.org/directory
+manual=true
+manual-public-ip-logging-ok=true
+manual-auth-hook=/usr/lib/certhub/certbot-hooks/lexicon-auth
+manual-cleanup-hook=/usr/lib/certhub/certbot-hooks/lexicon-cleanup
+preferred-challenges=dns-01
+agree-tos=true
+register-unsafely-without-email=true
+EOF
